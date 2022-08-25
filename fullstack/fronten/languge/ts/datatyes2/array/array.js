4ae69a6ade1cef2c8b11b4b340dfd8a01c35648b
wa5 <waseemslyar@gmail.com>
2022-08-25 22:14:47 +0530

    gffgfgghhg

---
+++ fullstack/fronten/languge/ts/datatyes2/array/array.js
@@ -0,0 +1,23 @@
+var fruites = ['applw', 'boll', 'cat'];
+var g = [];
+function usuli() {
+    var j = document.getElementById('abc');
+    var k = document.getElementById('abc').value;
+    j.value;
+    console.log(j.value);
+    console.log(g.push(k));
+    console.log(g);
+}
+var a = 10;
+var b = 23;
+var c = 78;
+var arr1 = [10, 20, 30];
+console.log(a);
+console.log(fruites[0]);
+var mobile = {
+    screen: 5,
+    speekr: "is there",
+    company: ['sony', 'lenovo'],
+    items: { speeker: { lenovo: 'wire', sony: 'magnet' } }
+};
+console.log(mobile);
---
+++ fullstack/fronten/languge/ts/datatyes2/array/array.ts
@@ -0,0 +1,28 @@
+var fruites:string[]=['applw','boll','cat']
+var g=[]
+function usuli(){
+    var j=document.getElementById('abc') as HTMLInputElement
+    var k=(<HTMLInputElement>document.getElementById('abc')).value;
+    j.value
+    console.log(j.value)
+    console.log(g.push(k))
+    console.log(g)
+}
+
+var a:Number=10
+var b:Number=23
+var c:Number=78
+var arr1=[10,20,30]
+
+console.log(a)
+console.log(fruites[0])
+var mobile={
+    screen:5,
+    speekr:"is there",
+    company:['sony','lenovo'],
+    items:{speeker:{lenovo:'wire',sony:'magnet'}}
+}
+console.log(mobile)
+
+
+
